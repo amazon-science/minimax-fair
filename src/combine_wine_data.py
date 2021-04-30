3241e2fd6da7a2524f7ac2d38338961cf5852109
gilwesle-aws <75460748+gilwesle-aws@users.noreply.github.com>
2021-04-29 21:49:07 -0400

    Update combine_wine_data.py

--- src/combine_wine_data.py
+++ src/combine_wine_data.py
@@ -4,9 +4,11 @@
 import numpy as np
 import pandas as pd
 
-df1 = pd.read_csv('../datasets/winequality-white.csv')
-df2 = pd.read_csv('../datasets/winequality-red.csv')
+df1 = pd.read_csv('../datasets/winequality-white.csv', sep=';')
+df2 = pd.read_csv('../datasets/winequality-red.csv', sep=';')
 
 df = pd.concat([df1, df2], ignore_index=True)
 
-df.to_csv('../datasets/winequality-full.csv')
+df['color'] = ['white' if i < df1.shape[0] else 'red' for i in range(df1.shape[0] + df2.shape[0])]
+
+df.to_csv('../datasets/winequality-full.csv', index=False)
